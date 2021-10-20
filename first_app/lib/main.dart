37e7b22d37645dcf316dd6e09e7a4bbfe7a99a8b
Khunakorn 6302037095 <khunakorn-sus63@tbs.tu.ac.th>
2021-10-20 10:22:40 +0700

    HTTP Client + Services + Controller

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:first_app/controllers/todo.dart';
 import 'package:first_app/model/first_form_model.dart';
+import 'package:first_app/pages/todo_page.dart';
+import 'package:first_app/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/eighth_page.dart';
@@ -12,17 +15,36 @@ import 'pages/third_page.dart';
 
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => FirstFormModel(),
-        ),
-      ],
-      child: MyApp(),
-    ),
-  );
+  var services = HttpServices();
+  var controller = TodoController(services);
+
+  runApp(TodoApp(controller: controller));
 }
+
+class TodoApp extends StatelessWidget {
+  final TodoController controller;
+  TodoApp({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoPage(
+        controller: controller,
+      ),
+    );
+  }
+}
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(
+//           create: (context) => FirstFormModel(),
+//         ),
+//       ],
+//       child: MyApp(),
+//     ),
+//   );
+// }
 
 class MyApp extends StatelessWidget {
   @override
